fb74aa5ae61f7ebae36e6754c238adb2d10883d4
deejay <aabebefe@yahoo.com>
2023-10-12 09:57:14 +0100

    First commit

---
+++ data_extraction.py
@@ -0,0 +1,9 @@
+class DataExtractor:
+    def __init__(self):
+      # add initialization code   
+     def extract_from_csv(self,file_path):
+        pass
+     def extract_from_api(self,url):
+        pass
+     def extract_from_s3(self,bucket_name,object_key):
+        pass
